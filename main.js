e569457a439783ee4f2b8b9509cb1562b573236a
hwaG <manhhoangg2k2@gmail.com>
2023-03-02 16:39:07 +0700

    hello

---
+++ main.js
@@ -0,0 +1,20 @@
+function showElement(){
+    document.getElementById('myBtn').classList.remove('hidden');
+}
+
+// function playMusic(){
+//     document.getElementById('music').play();
+// }
+
+
+setTimeout(showElement,10000);
+
+const btn=document.getElementById('myBtn');
+const card=document.getElementById('card');
+btn.addEventListener('click',function(){
+    document.getElementById('audio').play();
+    card.style.display = 'block';
+    document.getElementById('music').play();
+});
+
+
